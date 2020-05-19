0cd7c2a6cb990089d245fd5a5a6c4f6f53ee28a7
RulaAbuHasna <rulahasna99@gmail.com>
2020-05-19 17:57:44 +0300

    attempt2

--- soloProject/solo.js
+++ soloProject/solo.js
@@ -1,18 +1,18 @@
 var books=[
 {
- name: "my book",
- author:"my author",
- cost:30
-},
-{
- name: "my book2",
- author:"my author2",
+ name: "As you like it",
+ author:"William Shakespeare",
  cost:40
 },
 {
- name: "my book3",
- author:"my author3",
- cost:50
+ name: "The Alchemist",
+ author:"Paulo Coelho",
+ cost:35
+},
+{
+ name: "1Q84",
+ author:"Murakami",
+ cost:70
 },
 {
  name: "my book4",
@@ -20,24 +20,70 @@ var books=[
  cost:60
 } 
 ]
-
+//displaying the books when pressed
 $('#btn').click(function displayBook(){
 	$('div').css("display","block");
 
-	$('#book1').text(books[0].name+" the author "+books[0].author+" the cost "+books[0].cost);
-	$('#book2').text(books[1].name+" the author "+books[1].author+" the cost "+books[1].cost);
-	$('#book3').text(books[2].name+" the author "+books[2].author+" the cost "+books[2].cost);
-	$('#book4').text(books[3].name+" the author "+books[3].author+" the cost "+books[3].cost);
+	$('#book1').text("\""+books[0].name+"\""+" for "+books[0].author+", costs: "+books[0].cost +"$");
+	$('#book2').text("\""+books[1].name+"\""+" for "+books[1].author+", costs: "+books[1].cost +"$");
+    $('#book3').text("\""+books[2].name+"\""+" for "+books[2].author+", costs: "+books[2].cost +"$");
+    $('#book4').text("\""+books[3].name+"\""+" for "+books[3].author+", costs: "+books[3].cost +"$");
 })
 
+//add the books to my list 
 $('#add').click(function(){
+   if($(":checkbox:checked").length !== 0){
 	if($('#first').prop("checked") === true ){
-		alert("its working");
+		var li=$("<li></li>");
+		li.text(books[0].name);
+		$("ul").append(li);
 	}
+	if($('#second').prop("checked") === true ){
+        var li=$("<li></li>");
+		li.text(books[1].name);
+		$("ul").append(li);
+	}
+	if($('#third').prop("checked") === true ){
+         var li=$("<li></li>");
+		li.text(books[2].name);
+		$("ul").append(li);
+	}
+	if($('#fourth').prop("checked") === true ){
+        var li=$("<li></li>");
+		li.text(books[3].name);
+		$("ul").append(li);
+	}}else{
+		alert("please pick some books :D");
+	}
+    $("#first").prop("checked",false);
+    $("#second").prop("checked",false);
+    $("#third").prop("checked",false);
+    $("#fourth").prop("checked",false);
+
+})
+
+//
+$("#show").click(function displayList(){
+	$("#list").css("display","block");
+	$("small").css("display","block");
+})
+
+$("#hide").click(function hideList(){
+	$("#list").css("display","none");
+	$("small").css("display","none");
 })
 
 
+$("#remove").click(function removeLast(){
+	if($('li').length === 0){
+		alert("There are no books remove");
+	}else{
+    $('li').last().remove();
+}
+})
 
-
+$('ul').click(function(event){
+  $(event.target).remove();
+})
 
 
